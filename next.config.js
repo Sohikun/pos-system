ba127f000bce5863d949ff9ac95d18330059b7d4
Sohikun <sohaibbaig122@gmail.com>
2025-08-02 11:36:27 +0500

    Initial commit

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  // Added comment to trigger redeploy
-};
-module.exports = nextConfig;
